462517a36183d1238a918d924f4423625fa9cc61
nburrus <nburrus@a5c97724-9dca-4da3-bed3-1a0bcdcfa555>
2011-07-07 19:47:34 +0000

    Fix ICP.
    git-svn-id: https://handle2.uc3m.es/nestk/trunk@1333 a5c97724-9dca-4da3-bed3-1a0bcdcfa555

--- ntk/geometry/relative_pose_estimator_icp.cpp
+++ ntk/geometry/relative_pose_estimator_icp.cpp
@@ -56,14 +56,17 @@ bool RelativePoseEstimatorICP :: estimateNewPose(const RGBDImage& image)
     IterativeClosestPoint<PointXYZ, PointXYZ> reg;
     reg.setMaximumIterations (20);
     reg.setTransformationEpsilon (1e-5);
-    reg.setMaxCorrespondenceDistance (0.02);
+    reg.setMaxCorrespondenceDistance (0.05);
     reg.setInputCloud (cloud_source_ptr);
     reg.setInputTarget (cloud_target_ptr);
+
     reg.align (cloud_reg);
+    ntk_dbg_print(cloud.points[0].x, 1);
+    ntk_dbg_print(cloud_reg.points[0].x, 1);
 
     if (!reg.hasConverged())
     {
-      ntk_dbg(1) << "ICP did not converge, ignoricloudng.";
+      ntk_dbg(1) << "ICP did not converge, ignoring.";
       return false;
     }
 
@@ -71,19 +74,23 @@ bool RelativePoseEstimatorICP :: estimateNewPose(const RGBDImage& image)
     cv::Mat1f T(4,4);
     //toOpencv(t,T);
     for (int r = 0; r < 4; ++r)
-      for (int c = 0; c < 4; ++c)
-        T(r,c) = t(r,c);
+        for (int c = 0; c < 4; ++c)
+            T(r,c) = t(r,c);
 
     Pose3D icp_pose;
     icp_pose.setCameraTransform(T);
-
     ntk_dbg_print(icp_pose.cvTranslation(), 1);
 
+    // Pose3D stores the transformation from 3D space to image.
+    // So we have to invert everything so that unprojecting from
+    // image to 3D gives the right transformation.
+    // H2' = ICP * H1'
     m_current_pose.resetCameraTransform();
     m_current_pose = *image.calibration()->depth_pose;
+    m_current_pose.invert();
     m_current_pose.applyTransformAfter(icp_pose);
-
-    ntk_dbg_print(m_current_pose.cvTranslation(), 1);
+    m_current_pose.invert();
+    rgbdImageToPointCloud(cloud, image, m_current_pose);
     return true;
 }
 
