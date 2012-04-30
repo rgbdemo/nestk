6ca5195bb05e70fd4f59739380b3ad06441d818c
Nicolas Burrus <nicolas.burrus@manctl.com>
2012-04-29 18:35:57 -0700

    Minor cleanup.

--- ntk/detection/table_object_detector.h
+++ ntk/detection/table_object_detector.h
@@ -29,19 +29,19 @@
 #include <ntk/mesh/pcl_utils.h>
 #include <ntk/geometry/plane.h>
 
-#include "pcl/filters/voxel_grid.h"
-#include "pcl/filters/passthrough.h"
-#include "pcl/filters/extract_indices.h"
-#include "pcl/features/normal_3d.h"
-#include "pcl/kdtree/kdtree.h"
-#include "pcl/kdtree/kdtree_flann.h"
-#include "pcl/sample_consensus/method_types.h"
-#include "pcl/sample_consensus/model_types.h"
-#include "pcl/segmentation/sac_segmentation.h"
-#include "pcl/filters/project_inliers.h"
-#include "pcl/surface/convex_hull.h"
-#include "pcl/segmentation/extract_polygonal_prism_data.h"
-#include "pcl/segmentation/extract_clusters.h"
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/surface/convex_hull.h>
+#include <pcl/segmentation/extract_polygonal_prism_data.h>
+#include <pcl/segmentation/extract_clusters.h>
 
 namespace ntk
 {
