e60802cd1f34d5e50d2ba7afc4ae7b9ae8eb3690
Nicolas Burrus <nicolas.burrus@manctl.com>
2012-07-04 03:07:30 -0700

    Add missing header.

--- ntk/camera/rgbd_processor.cpp
+++ ntk/camera/rgbd_processor.cpp
@@ -24,6 +24,8 @@
 #include <ntk/image/bilateral_filter.h>
 #include <ntk/camera/calibration.h>
 
+#include <opencv2/photo/photo.hpp>
+
 #ifdef NESTK_USE_PCL
 #include <ntk/mesh/pcl_utils.h>
 #include <pcl/features/integral_image_normal.h>
